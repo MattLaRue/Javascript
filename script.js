ca9b10b1fb4b1b2b69ef55f9c7154be03acedba9
MattLaRue <laruemd@gmail.com>
2015-12-25 14:13:45 -0600

    If/Else + Math class

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 var age = prompt("What is your age?");
-var leap = age/4;
-var days = (age * 365 + leap) + " days old";
-alert(age + " is roughly " + days)
+// var leap = age/4;
+// var days = (age * 365 + leap) + " days old";
+// alert(age + " is roughly " + days)
+
+if (age < 0)
+{
+	alert("Please enter a valid age");
+}
+if (age == 21)
+{
+	alert("Happy 21st Birthday");
+}
+if (Math.sqrt(age) % 2 == 1 || Math.sqrt(age) % 2 == 0)
+{
+	alert("Perfect square");
+}
+else {
+	alert("Hi");
+}
